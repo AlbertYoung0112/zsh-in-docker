e314d9d2156751d8ca00d88191b019741e4f5b94
invY <25458585+AlbertYoung0112@users.noreply.github.com>
2021-01-18 16:44:37 +0800

    Update zsh-in-docker.sh

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -121,7 +121,7 @@ cd /tmp
 
 # Install On-My-Zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
-    proxychains4 sh -c "$(curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+    sh -c "$(proxychains4 curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
 fi
 
 # Generate plugin list
